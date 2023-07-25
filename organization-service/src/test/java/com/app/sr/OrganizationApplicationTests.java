6071eba3406cb2bed0e2a3e72ddd364d1a003759
eslamelshenawy <eslamelshenawy9316@gmail.com>
2023-07-25 13:56:37 +0300

    first commit

---
+++ organization-service/src/test/java/com/app/sr/OrganizationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.app.sr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SampleRoomApplicationTests.class)
+class SampleRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
